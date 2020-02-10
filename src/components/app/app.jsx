79814fcb73d11d42071dc18eff56b0d64e9ea241
k5va <k5va@yandex.ru>
2020-02-10 18:03:53 +0300

    Removed .jsx from imports

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import Main from "../main/main.jsx";
+import Main from "../main/main";
 
 const App = ({promoMovie, movies, movieTitleClickHandler}) => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/app/app.jsx";
+import App from "./components/app/app";
 import {movies, promoMovie} from "./mocks";
 
 const movieTitleClickHandler = () => {};
