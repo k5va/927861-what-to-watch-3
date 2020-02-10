053da4290773a31c0bccbac58f7cc4fbe105898c
k5va <k5va@yandex.ru>
2020-02-10 18:29:35 +0300

    Removed extra imports from tests

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -1,6 +1,4 @@
-import React from "react";
-import Enzyme, {shallow} from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import {shallow} from "enzyme";
 import Main from "./main.jsx";
 
 const movies = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`];
@@ -9,10 +7,6 @@ const promoMovie = {
   genre: `Drama`,
   date: 2014
 };
-
-Enzyme.configure({
-  adapter: new Adapter(),
-});
 
 it(`Should movies title be clicked`, () => {
   const movieTitleClickHandler = jest.fn();
