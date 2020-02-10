1035b2e63f89362b85e60fa815ca888b32ad916d
k5va <k5va@yandex.ru>
2020-02-10 18:26:08 +0300

    Added ProvidePlugin to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require(`path`);
+const webpack = require(`webpack`);
 
 module.exports = {
   entry: `./src/index.js`,
@@ -25,5 +26,11 @@ module.exports = {
   devtool: `source-map`,
   resolve: {
     extensions: [`.js`, `.jsx`]
-  }
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      'React': `react`,
+      'PropTypes': `prop-types`
+    })
+  ]
 };
