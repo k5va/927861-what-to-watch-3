907d5c8ace1a6e3a3e6da24cabab194de535999f
k5va <k5va@yandex.ru>
2020-02-13 19:04:07 +0300

    Updated App snapshot test

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,6 +1,19 @@
 import App from "./app";
 
-const movies = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`];
+const movies = [
+  {
+    title: `Movie 1`,
+    src: `pic 1`
+  },
+  {
+    title: `Movie 2`,
+    src: `pic 2`
+  },
+  {
+    title: `Movie 3`,
+    src: `pic 3`
+  }
+];
 const promoMovie = {
   name: `The Grand Budapest Hotel`,
   genre: `Drama`,
