e32855ba615dd39cc2f055f4567a73593dc6375e
k5va <k5va@yandex.ru>
2020-02-13 19:46:09 +0300

    Changed shallow to mout in Main e2e test

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -1,21 +1,38 @@
-import {shallow} from "enzyme";
+import {mount} from "enzyme";
 import Main from "./main.jsx";
 
-const movies = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`];
+const movies = [
+  {
+    title: `Movie 1`,
+    src: `pic 1`
+  },
+  {
+    title: `Movie 2`,
+    src: `pic 2`
+  },
+  {
+    title: `Movie 3`,
+    src: `pic 3`
+  }
+];
+
 const promoMovie = {
   name: `The Grand Budapest Hotel`,
   genre: `Drama`,
   date: 2014
 };
 
+const mockEvent = {
+  preventDefault() {},
+};
+
 it(`Should movies title be clicked`, () => {
   const handleMovieTitleClick = jest.fn();
-  const mainScreen = shallow(
+  const mainScreen = mount(
       <Main promoMovie={promoMovie} movies={movies} onMovieTitleClick={handleMovieTitleClick} />
   );
 
-  const movieTitles = mainScreen.find(`.small-movie-card__title`);
-  movieTitles.forEach((title) => title.props().onClick());
-
+  const movieTitles = mainScreen.find(`.small-movie-card__link`);
+  movieTitles.forEach((title) => title.simulate(`click`, mockEvent));
   expect(handleMovieTitleClick.mock.calls.length).toBe(movies.length);
 });
