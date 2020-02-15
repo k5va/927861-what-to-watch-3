85f6967764d9361d99434542d1a3e541d98ccefd
k5va <k5va@yandex.ru>
2020-02-15 21:34:44 +0300

    Added MovieDetails

--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@ export {default as App} from "./app/app";
 export {default as Main} from "./main/main";
 export {default as MoviesList} from "./movies-list/movies-list";
 export {default as MovieCard} from "./movie-card/movie-card";
+export {default as MovieDetails} from "./movie-details/movie-details";
