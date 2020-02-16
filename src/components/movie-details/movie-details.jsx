b508d21310de338b9e96483d004273476e8a7f53
k5va <k5va@yandex.ru>
2020-02-16 09:35:11 +0300

    Added props to MovieDetails

--- src/components/movie-details/movie-details.jsx
+++ src/components/movie-details/movie-details.jsx
@@ -1,10 +1,14 @@
 const MovieDetails = (props) => {
+  const {movie} = props;
+  const {title, genre, year, cover, poster, rating, description, director, actors} = movie;
+  const {score: ratingScore, count: ratingCount, level: ratingLevel} = rating;
+
   return (
     <>
     <section className="movie-card movie-card--full">
       <div className="movie-card__hero">
         <div className="movie-card__bg">
-          <img src="img/bg-the-grand-budapest-hotel.jpg" alt="The Grand Budapest Hotel" />
+          <img src={poster} alt={title} />
         </div>
 
         <h1 className="visually-hidden">WTW</h1>
@@ -27,10 +31,10 @@ const MovieDetails = (props) => {
 
         <div className="movie-card__wrap">
           <div className="movie-card__desc">
-            <h2 className="movie-card__title">The Grand Budapest Hotel</h2>
+            <h2 className="movie-card__title">{title}</h2>
             <p className="movie-card__meta">
-              <span className="movie-card__genre">Drama</span>
-              <span className="movie-card__year">2014</span>
+              <span className="movie-card__genre">{genre}</span>
+              <span className="movie-card__year">{year}</span>
             </p>
 
             <div className="movie-card__buttons">
@@ -55,8 +59,8 @@ const MovieDetails = (props) => {
       <div className="movie-card__wrap movie-card__translate-top">
         <div className="movie-card__info">
           <div className="movie-card__poster movie-card__poster--big">
-            <img src="img/the-grand-budapest-hotel-poster.jpg"
-              alt="The Grand Budapest Hotel poster" width="218" height="327" />
+            <img src={cover}
+              alt={title} width="218" height="327" />
           </div>
 
           <div className="movie-card__desc">
@@ -75,21 +79,17 @@ const MovieDetails = (props) => {
             </nav>
 
             <div className="movie-rating">
-              <div className="movie-rating__score">8,9</div>
+              <div className="movie-rating__score">{ratingScore}</div>
               <p className="movie-rating__meta">
-                <span className="movie-rating__level">Very good</span>
-                <span className="movie-rating__count">240 ratings</span>
+                <span className="movie-rating__level">{ratingLevel}</span>
+                <span className="movie-rating__count">{ratingCount} ratings</span>
               </p>
             </div>
 
             <div className="movie-card__text">
-              <p>In the 1930s, the Grand Budapest Hotel is a popular European ski resort, presided over by concierge Gustave H. (Ralph Fiennes). Zero, a junior lobby boy, becomes Gustave's friend and protege.</p>
-
-              <p>Gustave prides himself on providing first-className service to the hotel's guests, including satisfying the sexual needs of the many elderly women who stay there. When one of Gustave's lovers dies mysteriously, Gustave finds himself the recipient of a priceless painting and the chief suspect in her murder.</p>
-
-              <p className="movie-card__director"><strong>Director: Wes Andreson</strong></p>
-
-              <p className="movie-card__starring"><strong>Starring: Bill Murray, Edward Norton, Jude Law, Willem Dafoe and other</strong></p>
+              <p>{description}</p>
+              <p className="movie-card__director"><strong>Director: {director}</strong></p>
+              <p className="movie-card__starring"><strong>Starring: {actors} and other</strong></p>
             </div>
           </div>
         </div>
@@ -157,6 +157,24 @@ const MovieDetails = (props) => {
     </div>
     </>
   );
+};
+
+MovieDetails.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    year: PropTypes.number.isRequired,
+    cover: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+    actors: PropTypes.arrayOf(PropTypes.string).isRequired,
+    rating: PropTypes.shape({
+      score: PropTypes.number.isRequired,
+      count: PropTypes.number.isRequired,
+      level: PropTypes.string.isRequired
+    }).isRequired
+  }).isRequired
 };
 
 export default MovieDetails;
