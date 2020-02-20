06a0c78b8665cba58cd93466977d5536836fa9a6
k5va <k5va@yandex.ru>
2020-02-20 13:16:34 +0300

    Updated MovieCard to use VideoPlayer

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,31 +1,55 @@
-const MovieCard = ({movie, onHover, onClick}) => {
-  const {title, cover} = movie;
-  return (
-    <article
-      className="small-movie-card catalog__movies-card"
-      onMouseOver={() => onHover(movie)}
-      onClick={() => onClick(movie)}
-    >
-      <div className="small-movie-card__image">
-        <img src={cover} alt={title} width="280" height="175" />
-      </div>
-      <h3 className="small-movie-card__title">
-        <a
-          className="small-movie-card__link"
-          href="movie-page.html"
-          onClick={(evt) => evt.preventDefault()}
-        >
-          {title}
-        </a>
-      </h3>
-    </article>
-  );
-};
+import {VideoPlayer} from "@components";
+
+class MovieCard extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPlaying: false
+    };
+  }
+
+  render() {
+    const {movie, onHover, onClick} = this.props;
+    const {title, cover, src} = movie;
+    const {isPlaying} = this.state;
+
+    return (
+      <article
+        className="small-movie-card catalog__movies-card"
+        onMouseOver={() => onHover(movie)}
+        onClick={() => onClick(movie)}
+        onMouseEnter={() => setTimeout(() => {
+          this.setState({isPlaying: true});
+        }, 1000)}
+        onMouseLeave={() => this.setState({isPlaying: false})}
+      >
+        <div className="small-movie-card__image">
+          <VideoPlayer
+            src={src}
+            poster={cover}
+            isPlaying={isPlaying}
+          />
+        </div>
+        <h3 className="small-movie-card__title">
+          <a
+            className="small-movie-card__link"
+            href="movie-page.html"
+            onClick={(evt) => evt.preventDefault()}
+          >
+            {title}
+          </a>
+        </h3>
+      </article>
+    );
+  }
+}
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    cover: PropTypes.string.isRequired
+    cover: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired
   }).isRequired,
   onClick: PropTypes.func.isRequired,
   onHover: PropTypes.func.isRequired
