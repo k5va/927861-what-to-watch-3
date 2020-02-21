dedb3304a854db04442d122710e71900776df20d
k5va <k5va@yandex.ru>
2020-02-21 17:45:14 +0300

    Added getSimilarMovies to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,3 @@
 export {default as generateId} from "./generate-id";
 export {default as convertScoreToText} from "./convert-score-to-text";
+export {default as getSimilarMovies} from "./get-similar-movies";
