7577b4677197e34cdc0317d1184e71b5e4c131de
k5va <k5va@yandex.ru>
2020-02-24 15:16:20 +0300

    Created redux store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import ReactDOM from "react-dom";
 import {App} from "@components";
 import {movies, promoMovie} from "./mocks";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "./reducer";
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
-    <App
-      promoMovie={promoMovie}
-      movies={movies}
-    />,
+    <Provider store={store}>
+      <App
+        promoMovie={promoMovie}
+        movies={movies}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
