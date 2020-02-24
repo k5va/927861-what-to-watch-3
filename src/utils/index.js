761b9c7701f5cb13150540d7d0de422e43653d2e
k5va <k5va@yandex.ru>
2020-02-24 15:18:42 +0300

    Added getUniqueGenres to utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,6 @@
 export {default as generateId} from "./generate-id";
 export {default as convertScoreToText} from "./convert-score-to-text";
 export {default as getSimilarMovies} from "./get-similar-movies";
+export {default as extend} from "./extend";
+export {default as getUniqueGenres} from "./get-unique-genres";
+export {default as filterMoviesByGenre} from "./filter-movies-by-genre";
