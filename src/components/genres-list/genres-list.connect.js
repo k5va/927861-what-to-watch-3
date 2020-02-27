4bb20681490dc2ceb6e0d88ffdb2baaa61f417c9
k5va <k5va@yandex.ru>
2020-02-27 16:23:05 +0300

    Changed props of genres list component

--- src/components/genres-list/genres-list.connect.js
+++ src/components/genres-list/genres-list.connect.js
@@ -1,16 +1,18 @@
 import {connect} from "react-redux";
 import {ActionCreator} from "@store";
 import GenresList from "./genres-list";
+import getUniqueGenres from "./selectors/get-unique-genres";
 
-const mapStateToProps = (state) => ({
-  movies: state.movies,
-  selectedGenre: state.selectedGenre
+const mapStateToProps = ({movies, selectedGenre}) => ({
+  genres: getUniqueGenres(movies),
+  selectedGenre
 });
 
 const mapDispatchToProps = (dispatch) => ({
   onGenreSelect(genre) {
     dispatch(ActionCreator.changeGenre(genre));
     dispatch(ActionCreator.filterMoviesByGenre());
+    dispatch(ActionCreator.resetShowMoreMovies());
   }
 });
 
