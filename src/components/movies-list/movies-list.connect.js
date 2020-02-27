4d366da5a8bfe64a47ed2bb69acd5e18d955e0d8
k5va <k5va@yandex.ru>
2020-02-27 16:25:42 +0300

    Updated movies-list.connect to use filteredMovies from store

--- src/components/movies-list/movies-list.connect.js
+++ src/components/movies-list/movies-list.connect.js
@@ -1,12 +1,11 @@
 import {connect} from "react-redux";
 import MoviesList from "./movies-list";
-import filterMoviesByGenre from "./selectors/filter-movies-by-genre";
 import getSimilarMovies from "./selectors/get-similar-movies";
 
-const mapStateToProps = (state) => ({
-  movies: state.selectedMovie ?
-    getSimilarMovies(state.selectedMovie, state.movies) :
-    filterMoviesByGenre(state.movies, state.selectedGenre)
+const mapStateToProps = ({selectedMovie, movies, filteredMovies, shownMoviesNumber}) => ({
+  movies: selectedMovie ?
+    getSimilarMovies(selectedMovie, movies) :
+    filteredMovies.slice(0, shownMoviesNumber)
 });
 
 const mapDispatchToProps = () => ({
