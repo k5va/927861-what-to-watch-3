aa9f2fa2a58deae965eb7db9e29453c119c3b921
k5va <k5va@yandex.ru>
2020-03-02 20:22:19 +0300

    Added VideoPlayerFull snapshot test

---
+++ src/components/video-player-full/video-player-full.test.js
@@ -0,0 +1,20 @@
+import {VideoPlayerFull} from "@components";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import {reducer} from "@store";
+
+const store = createStore(reducer);
+
+it(`VideoPlayerFull should render correctly`, () => {
+  const wrapper = renderer
+    .create(
+        <Provider store={store}>
+          <VideoPlayerFull isPlaying={true} />
+        </Provider>,
+        {
+          createNodeMock: () => ({})
+        }
+    )
+    .toJSON();
+  expect(wrapper).toMatchSnapshot();
+});
