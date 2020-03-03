52e00dc20b26ed229a862d10b2805aa334397253
k5va <k5va@yandex.ru>
2020-03-03 17:06:47 +0300

    Fixed video player e2e test

--- src/components/video-player/video-player.e2e.test.js
+++ src/components/video-player/video-player.e2e.test.js
@@ -5,11 +5,21 @@ const movie = {
   src: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`,
 };
 
-it(`VideoPlayer should have two states: Playing, Paused`, () => {
+it(`VideoPlayer should have Playing state`, () => {
+  jest
+    .spyOn(HTMLMediaElement.prototype, `play`)
+    .mockImplementationOnce(() => {});
+
   const wrapperPlaying = mount(
       <VideoPlayer src={movie.src} poster={movie.cover} isPlaying={true} />
   );
   expect(wrapperPlaying.state().isPlaying).toBe(true);
+});
+
+it(`VideoPlayer should have Paused state`, () => {
+  jest
+    .spyOn(HTMLMediaElement.prototype, `pause`)
+    .mockImplementationOnce(() => { });
 
   const wrapperPaused = mount(
       <VideoPlayer src={movie.src} poster={movie.cover} isPlaying={false} />
