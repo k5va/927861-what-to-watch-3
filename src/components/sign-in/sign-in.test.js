597ffbdff3d98036341d450014451053a68b66b7
k5va <k5va@yandex.ru>
2020-03-10 18:06:26 +0300

    Added SignIn snapshot test

---
+++ src/components/sign-in/sign-in.test.js
@@ -0,0 +1,14 @@
+import {SignIn} from "@components";
+import {Router} from "react-router-dom";
+import {history} from "@routes";
+
+it(`SignIn should render correctly`, () => {
+  const wrapper = renderer
+    .create(
+        <Router history={history}>
+          <SignIn onSubmit={() => {}} />
+        </Router>
+    )
+    .toJSON();
+  expect(wrapper).toMatchSnapshot();
+});
