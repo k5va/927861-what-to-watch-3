0a71300dd33389112ebc1c157901409855bf191c
k5va <k5va@yandex.ru>
2020-03-23 14:25:37 +0300

    Added tests to app reducer

--- src/store/reducer/app/app.test.js
+++ src/store/reducer/app/app.test.js
@@ -33,3 +33,35 @@ it(`Reducer should change App state`, () => {
   expect(reducer(initialState, ActionCreator.changeAppState(AppState.READY)))
     .toEqual(targetState);
 });
+
+it(`Reducer should show update more movies`, () => {
+  const initialState = {
+    appState: AppState.READY,
+    selectedGenre: Genre.ALL,
+    shownMoviesNumber: DEFAULT_SHOWN_MOVIES_NUMBER
+  };
+  const targetState = {
+    appState: AppState.READY,
+    selectedGenre: Genre.ALL,
+    shownMoviesNumber: DEFAULT_SHOWN_MOVIES_NUMBER * 2
+  };
+
+  expect(reducer(initialState, ActionCreator.showMoreMovies(DEFAULT_SHOWN_MOVIES_NUMBER)))
+    .toEqual(targetState);
+});
+
+it(`Reducer should reset show more movies`, () => {
+  const initialState = {
+    appState: AppState.READY,
+    selectedGenre: Genre.ALL,
+    shownMoviesNumber: DEFAULT_SHOWN_MOVIES_NUMBER * 2
+  };
+  const targetState = {
+    appState: AppState.READY,
+    selectedGenre: Genre.ALL,
+    shownMoviesNumber: DEFAULT_SHOWN_MOVIES_NUMBER
+  };
+
+  expect(reducer(initialState, ActionCreator.resetShowMoreMovies()))
+    .toEqual(targetState);
+});
